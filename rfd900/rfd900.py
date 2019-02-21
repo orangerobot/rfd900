a07e73d30618759334888d7ef216b9a5a42745e3
Scott White <sww314@gmail.com>
2019-02-21 08:53:59 -0600

    feature: add support for rfd900 async firmware.

--- rfd900/rfd900.py
+++ rfd900/rfd900.py
@@ -25,8 +25,8 @@ parser.add_option(
 opts, args = parser.parse_args()
 
 # TODO: add validation
-# TODO: add all the commands
-COMMANDS = {
+# Multi-POINT COMMANDS
+MP_COMMANDS = {
     "air_speed": {"cmd": "ATS2={}", "validator": None},
     "netid": {"cmd": "ATS3={}", "validator": None},
     "txpower": {"cmd": "ATS4={}", "validator": None},
@@ -49,13 +49,49 @@ COMMANDS = {
     "nodecount13": {"cmd": "AT&M2=13,{}", "validator": None}, # max number of nodes in network 13
 }
 
+ASYNC_COMMANDS = {
+    "air_speed": {"cmd": "ATS2={}", "validator": None},
+    "max_data": {"cmd": "ATS3={}", "validator": None}, # max size of packet data section
+    "max_retries": {"cmd": "ATS4={}", "validator": None}, # max number of retries
+    "global_retries": {"cmd": "ATS5={}", "validator": None}, # number of retries for broadcast messages
+    "txencap": {"cmd": "ATS7={}", "validator": None},
+    "rxencap": {"cmd": "ATS8={}", "validator": None},
+    "netid": {"cmd": "ATS9={}", "validator": None},
+    "nodeid": {"cmd": "ATS10={}", "validator": None},
+    "destid": {"cmd": "ATS11={}", "validator": None},
+    "txpower": {"cmd": "ATS12={}", "validator": None},
+    "mavlink": {"cmd": "ATS13={}", "validator": None},
+    "min_freq": {"cmd": "ATS14={}", "validator": None},  # min frequency in kHz
+    "max_freq": {"cmd": "ATS15={}", "validator": None},  # max frequency in kHz
+    "num_channels": {"cmd": "ATS16={}", "validator": None},  # number of frequency hopping channels.
+    "rtscts": {"cmd": "ATS18={}", "validator": None},  # Ready to send and clear to send
+    "encryption_level": {"cmd": "ATS19={}", "validator": None},  # enable encryption
+    "encryption_key": {"cmd": "AT&E={}", "validator": None},  # set key only on 900x
+}
+
+def command(radio_type, key):
+    if radio_type == 'MP':
+        return MP_COMMANDS[key]["cmd"]
+    if radio_type == 'ASYNC':
+        return ASYNC_COMMANDS[key]["cmd"]
+
 if len(args) == 0:
     print("usage: rfd900.py <DEVICE...>")
     sys.exit(1)
 
 
 def parse_firmware(value):
-    print("Firmware is: ", value)
+    #RFD ASYNC 2.47 on RFD900xR1.1
+    # [2] RFD SiK 2.75MP on RFD900xR1.1
+    v = value.decode("utf-8")
+    if "ASYNC" in v:
+        return "ASYNC"
+    if "MP" in v:
+        return "MP"
+
+    print("Unknown Firmware version: ", value)
+    return None
+
 
 def load_config():
     """ Load the configuration """
@@ -66,9 +102,27 @@ def load_config():
     stream.close()
     return config
 
+def enter_command_mode(serial, reset=True):
+
+    try:
+        serial.send("+++")
+        time.sleep(1)
+        serial.expect("OK", timeout=1)
+        return True
+    except Exception as e:
+        print(e)
+
+    if reset: # maybe already in command mode?
+        serial.sendline("ATZ")
+        time.sleep(3)
+        return enter_command_mode(serial, reset=False)
+
+    return None
+
 
 def configure_radio(device, config):
     """Configure the radio"""
+    print(opts.baudrate)
     port = serial.Serial(
         device,
         opts.baudrate,
@@ -86,16 +140,18 @@ def configure_radio(device, config):
     )
     ser = fdpexpect.fdspawn(port.fileno(), logfile=fout)
     ser.linesep = b"\r\n"
-    ser.send("+++")
-    time.sleep(1)
-    ser.expect("OK")
+    if not enter_command_mode(ser):
+        print("unable to enter command mode. Exiting...")
+        exit()
+
     ser.send("ATI\r\n")
 
     # print("sent ATI:" , ser.before)
     try:
+        #RFD ASYNC 2.47 on RFD900xR1.1
         # [2] RFD SiK 2.75MP on RFD900xR1.1
         ser.expect("RFD900", timeout=2)
-        parse_firmware(ser.before)
+        radio_type = parse_firmware(ser.before)
     except fdpexpect.TIMEOUT:
         print("timeout")
         return
@@ -108,7 +164,7 @@ def configure_radio(device, config):
     to_radio = {**config["common"], **config[device_id.decode("utf-8")]}
     for key, value in to_radio.items():
         print("Setting {} to {}".format(key, value))
-        cmd = COMMANDS[key]["cmd"]
+        cmd = command(radio_type, key)
         ser.sendline(cmd.format(value).encode("ascii"))
         ser.expect("OK")
 
